3ec72af8e395304323371bf25898d12fa43209c4
Tim Stewart <timoteostewart1977@gmail.com>
2024-05-08 05:52:40 +0000

    edits

--- functions.sh
+++ functions.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# debugging switches
+# set -o errexit  # abort on nonzero exit status; same as set -e
+# set -o nounset  # abort on unbound variable; same as set -u
+# set -o pipefail # don't hide errors within pipes
+# set -o xtrace   # show commands being executed; same as set -x
+# set -o verbose  # verbose mode; same as set -v
+
 # usage: see locals below
 add-deb-repo() {
     # Check for missing parameters
@@ -14,7 +21,6 @@ add-deb-repo() {
     local distro="${3}"
     local component="${4}"
     local label="${5}"
-
     local keyring="/usr/share/keyrings/${label}-keyring.gpg"
     local sources="/etc/apt/sources.list.d/${label}.list"
 
@@ -38,12 +44,20 @@ cd-or-die() {
     cd "${1}" || die "Could not cd to ${1}."
 }
 
+# usage: count-network-interfaces
+# returns: the number of network interfaces, excluding the loopback interface
+count-network-interfaces() {
+    local broadcast_interfaces=($(get-list-of-network-interfaces))
+    IFS=' ' read -r -a broadcast_interfaces <<<"$(get-list-of-network-interfaces)"
+    echo "${#broadcast_interfaces[@]}"
+}
+
 # usage: die "$MESSAGE" ["$EXIT_CODE"]
 die() {
     local message="${1:-Unspecified Error}"
     local exit_code="${2:-1}"
-    printf >&2 "Error: ${message}\nAn unrecoverable error has occurred. Look above for any error messages.\n"
-    exit "$exit_code"
+    printf >&2 "Error: %s\nAn unrecoverable error has occurred. Look above for any error messages.\n" "${message}"
+    exit "${exit_code}"
 }
 
 # usage: die-if-file-not-present $FILENAME_WITH_PATH ["$MESSAGE"]
@@ -74,7 +88,14 @@ die-if-program-not-available() {
 ensure-dir-or-die() {
     [[ -z "${1}" ]] && die "ensure-dir-or-die() requires a directory to be specified."
     local dir="${1}"
-    mkdir -p "$dir" || die "Could not create directory ${dir}."
+    mkdir --parents "$dir" || die "Could not create directory ${dir}."
+}
+
+# usage: ensure-file-or-die "$FILE"
+ensure-file-or-die() {
+    [[ -z "${1}" ]] && die "ensure-file-or-die() requires a file to be specified."
+    local file="${1}"
+    touch "${file}" || die "Could not create file ${file}."
 }
 
 # usage: get-iso8601-date
@@ -102,12 +123,88 @@ get-list-of-network-interfaces() {
 
 # usage: get-time-in-unix-seconds
 get-time-in-unix-seconds() {
-    printf '%(%s)T\n' -1
+    printf $(date -u +%s)
 }
 
 # usage: make-beep
 make_beep() {
     printf "\a"
+}
+
+# usage: prefer $INTERFACE_NAME
+# example: prefer eth0
+# returns 0 if successful, 1 if not successful
+prefer() {
+
+    die-if-not-root
+
+    preferred_network_name="${1}"
+
+    if [[ -z "${preferred_network_name}" ]]; then
+        printf "Argument must be eth0 or eth1.\n"
+        exit 1
+    fi
+
+    if [[ "$1" != "eth0" ]] && [[ "$1" != "eth1" ]]; then
+        printf "Argument must be eth0 or eth1.\n"
+        exit 1
+    fi
+
+    is-ip-address-reachable() {
+        local ip_address="${1}"
+        if ping -c 4 -q "${ip_address}" >/dev/null; then
+            return 0
+        else
+            return 1
+        fi
+    }
+
+    update-metric-for-interface() {
+        local gateway_address="${1}"
+        local interface_name="${2}"
+        local metric="${3}"
+        ip route del default via "${gateway_address}" dev "${interface_name}"
+        ip route add default via "${gateway_address}" dev "${interface_name}" metric "${metric}"
+    }
+
+    make_preferred() {
+        local primary_interface="${1}"
+
+        if [[ "${primary_interface}" == "eth0" ]]; then
+            local primary_gateway="192.168.1.1"
+            local secondary_interface="eth1"
+            local secondary_gateway="192.168.2.254"
+        fi
+        if [[ "${primary_interface}" == "eth1" ]]; then
+            local primary_gateway="192.168.2.254"
+            local secondary_interface="eth0"
+            local secondary_gateway="192.168.1.1"
+        fi
+
+        update-metric-for-interface "${primary_gateway}" "${primary_interface}" 1000
+        # higher number means lower priority
+        update-metric-for-interface "${secondary_gateway}" "${secondary_interface}" 1024
+
+    }
+
+    # Define an array of IP addresses
+    ip_addresses=("192.168.1.1" "192.168.2.254")
+
+    # Loop through each IP address in the array
+    for ip in "${ip_addresses[@]}"; do
+        if ! is-ip-address-reachable "${ip}"; then
+            printf "%s not reachable!\n" "${ip}"
+            exit 1
+        fi
+    done
+
+    sleep 4
+
+    make_preferred "${preferred_network_name}"
+
+    wan_ip=$(curl --silent icanhazip.com)
+    printf "preferred interface: ${preferred_network_name}\n"
+    printf "wan_ip=${wan_ip}\n"
 }
 
 # usage: program-not-available $PROGRAM_NAME
@@ -126,11 +223,21 @@ program-available() {
     return 0
 }
 
+source-if-exists() {
+    local file=$1
+    if [[ -f "${file}" ]]; then
+        source "${file}"
+    else
+        printf >&2 "Error: Could not find '%s'.\n" "$file"
+        return 1
+    fi
+}
+
 trim_string() {
     local input=$1
     # Remove leading whitespace
     input="${input#"${input%%[![:space:]]*}"}"
     # Remove trailing whitespace
     input="${input%"${input##*[![:space:]]}"}"
-    echo "$input"
+    printf "${input}"
 }
